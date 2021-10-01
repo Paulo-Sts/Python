477e6128688272f05b17508581892aff3899c81e
Paulo Santos <pauloed_@outlook.com>
2021-10-01 12:40:08 -0300

    Função type

---
+++ Introducao/type.py
@@ -0,0 +1,10 @@
+#A função type() é usada para informar qual o tipo de uma variável/dado em python
+nome = "Paulo"
+idade = 23
+altura = 1.65
+maior_de_idade = True
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(maior_de_idade))
