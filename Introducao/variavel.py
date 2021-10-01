6503d0bf64cb92378e02f19ef5305819be553851
Paulo Santos <pauloed_@outlook.com>
2021-10-01 12:39:06 -0300

    Variaveis em python

---
+++ Introducao/variavel.py
@@ -0,0 +1,15 @@
+#As variáveis em python são tipadas dinâmicamente, não sendo necessário definir o tipo inicial
+
+#exemplo
+
+valor = 1 #tipo inteiro
+valor = 2.5 #tipo float
+valor = "Olá" #tipo string
+
+#A conversão em python para nome de variáveis é utilizar o snake_case
+#Os principais tipos primitivos em python são:
+numero_inteiro = 1 #int
+numero_decimal = 1.3 #float
+valor_booleano = True #bool
+string_de_caractere = "String" #string
+numero_complexo = 1 + 2j #complex
