19dba42d649a77164cc1a8e1a703b1215f3a2466
Paulo Santos <pauloed_@outlook.com>
2021-10-04 10:35:05 -0300

    Adicionando o while no jogo

--- Exercicios/adivinhacao.py
+++ Exercicios/adivinhacao.py
@@ -3,23 +3,28 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("=================================")
 
 numero_secreto = 20
-chute_str = input("Qual o número secreto?\n")
-chute = int(chute_str)
+tentativa = 1
+total_tentativas = 3
+while(tentativa <= total_tentativas):
+    print("Tentativa {} de {}".format(tentativa, total_tentativas))
+    chute_str = input("Qual o número secreto?\n")
+    chute = int(chute_str)
 
-print("Você digitou:", chute)
+    print("Você digitou:", chute)
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(acertou):
-    print("Parabéns Você Acertou!")
-else: 
-    if(maior):
-        print("O número que você digitou é maior que o número secreto!")
-    elif(menor):
-        print("O número que você digitou é menor que o número secreto!")
-    else:
-        print("Você Errou!")
+    if(acertou):
+        print("Parabéns Você Acertou!")
+    else: 
+        if(maior):
+            print("O número que você digitou é maior que o número secreto!")
+        elif(menor):
+            print("O número que você digitou é menor que o número secreto!")
+        else:
+            print("Você Errou!")
+    tentativa = tentativa + 1
 
 print("Fim de Jogo!")
