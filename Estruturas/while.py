2b4432df25f2b58b2175179e8abe4d561ef0663a
Paulo Santos <pauloed_@outlook.com>
2021-10-04 10:30:49 -0300

    while

---
+++ Estruturas/while.py
@@ -0,0 +1,15 @@
+#O while é uma estrutura que executa um bloco de código em loop enquanto uma condição é atendida
+print("================")
+print("RESULTADO PROVAS")
+print("================")
+
+notas = 1
+total_notas = 0
+while(notas <= 4):
+    recebe_nota_str = input("Digite sua nota:\n")
+    nota = float(recebe_nota_str)
+    total_notas = total_notas + nota
+    
+    notas = notas + 1
+media = total_notas / 4
+print("Sua média é: {}".format(media))
