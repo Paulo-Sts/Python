fff69f5f7913d93b9e522122aa1233696504fd42
Paulo Santos <pauloed_@outlook.com>
2021-10-04 10:27:58 -0300

    Interpolação de strings

---
+++ Introducao/interpolacao_strings.py
@@ -0,0 +1,7 @@
+#Uma das formas de concatenar strings é por meio da função format()
+
+nome = "Paulo"
+idade = 23
+profissao = "Programador"
+
+print("Oi meu nome é {}, tenho {} anos e sou {}.".format(nome, idade, profissao))
