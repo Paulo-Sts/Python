89e8968835f623a98788141b0e491bbaf619ed18
Paulo Santos <pauloed_@outlook.com>
2021-10-05 10:45:02 -0300

    Renomeando arquivo interpolação_strings

--- Introducao/interpolacao_strings.py
+++
@@ -1,7 +0,0 @@
-#Uma das formas de concatenar strings é por meio da função format()
-
-nome = "Paulo"
-idade = 23
-profissao = "Programador"
-
-print("Oi meu nome é {}, tenho {} anos e sou {}.".format(nome, idade, profissao))
