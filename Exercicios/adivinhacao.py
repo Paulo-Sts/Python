61349550d81b231faf237de3e8d87d6e4ac9f7bb
Paulo Santos <pauloed_@outlook.com>
2021-10-05 10:45:47 -0300

    Adicionando o for no exercicio

--- Exercicios/adivinhacao.py
+++ Exercicios/adivinhacao.py
@@ -3,21 +3,23 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("=================================")
 
 numero_secreto = 20
-tentativa = 1
 total_tentativas = 3
-while(tentativa <= total_tentativas):
+for tentativa in range(1,total_tentativas + 1):
     print("Tentativa {} de {}".format(tentativa, total_tentativas))
-    chute_str = input("Qual o número secreto?\n")
+    chute_str = input("Digite um número entre 1 e 100:\n")
     chute = int(chute_str)
 
     print("Você digitou:", chute)
-
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if(acertou):
         print("Parabéns Você Acertou!")
+        break
     else: 
         if(maior):
             print("O número que você digitou é maior que o número secreto!")
@@ -25,6 +27,5 @@ while(tentativa <= total_tentativas):
             print("O número que você digitou é menor que o número secreto!")
         else:
             print("Você Errou!")
-    tentativa = tentativa + 1
 
 print("Fim de Jogo!")
