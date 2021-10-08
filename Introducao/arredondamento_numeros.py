d7a44899778f838ade738e44d130803a94dbe9b6
Paulo Santos <pauloed_@outlook.com>
2021-10-08 11:16:08 -0300

    função round

---
+++ Introducao/arredondamento_numeros.py
@@ -0,0 +1,6 @@
+#A função round é uma função interna do python que arredonda valores de ponto flutuante
+numero = 3.4
+numero2 = 6.6
+numero3 = 9.5
+
+print(round(numero), round(numero2), round(numero3))
