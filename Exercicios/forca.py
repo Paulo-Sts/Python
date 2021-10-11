f108bbca297ae92a399bb09e86e64c022c0a0974
Paulo Santos <pauloed_@outlook.com>
2021-10-11 12:05:48 -0300

    Desenvolvendo o jogo da forca

--- Exercicios/forca.py
+++ Exercicios/forca.py
@@ -3,6 +3,24 @@ def jogar():
     print("Bem vindo ao jogo da forca!")
     print("===========================")
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+    while(not enforcou and not acertou):
+       
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+        print(letras_acertadas)
+
     print("Fim de Jogo!")
 
 if(__name__ == "__main__"):
