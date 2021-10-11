d5ae2337b6283d04841e1e376f9bcce24d1d7374
Paulo Santos <pauloed_@outlook.com>
2021-10-11 12:04:29 -0300

    Definindo tipos primitivos em python

---
+++ Introducao/tipos_primitivos.py
@@ -0,0 +1,6 @@
+#Os principais tipos primitivos em python são:
+numero_inteiro = 1 #int
+numero_decimal = 1.3 #float
+valor_booleano = True #bool
+string_de_caractere = "String" #string
+numero_complexo = 1 + 2j #complex
