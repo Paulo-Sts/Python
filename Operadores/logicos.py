9e29c717e0a5a379077a689644c2f9b348322e1f
Paulo Santos <pauloed_@outlook.com>
2021-10-11 12:01:36 -0300

    Operadores lógicos em python

---
+++ Operadores/logicos.py
@@ -0,0 +1,8 @@
+#Os operadores lógicos retornam um tipo booleano sendo True ou False
+
+valor_1 = True
+valor_2 = False
+
+print(valor_1 and valor_2) #Operador de conjunção ou E
+print(valor_1 or valor_2) #Operador de disjunção ou OU
+print(valor_1 and not valor_2) #Operador de negação ou NÃO
