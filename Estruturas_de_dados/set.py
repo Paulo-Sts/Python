0e0db18d70fe9281d7e353ac59796a5d81b16fc2
Paulo Santos <pauloed_@outlook.com>
2021-10-13 12:12:01 -0300

    Conjuntos em python

---
+++ Estruturas_de_dados/set.py
@@ -0,0 +1,5 @@
+#Set é uma coleção de dados não ordenada que representa um conjunto e por isso não aceita dados duplicados.
+numeros_cpf = {11211211211, 45698700123}
+
+numeros_cpf.add(10010010011)
+print(numeros_cpf)
