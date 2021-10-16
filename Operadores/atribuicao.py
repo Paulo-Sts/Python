ea3b8fded0f16b9afb8ec74834558aa0a7a59b10
Paulo Santos <pauloed_@outlook.com>
2021-10-16 11:45:59 -0300

    Operadores de atribuição

---
+++ Operadores/atribuicao.py
@@ -0,0 +1,17 @@
+x = 1 #Atribui o valor 1 a variável x
+print(x) 
+
+x += 9 #Adiciona 9
+print(x) 
+
+x -= 1 #Subtrai 1
+print(x) 
+
+x *= 2 #Multiplica por 2
+print(x) 
+
+x /= 2 #Divide por 2
+print(x) 
+
+x %= 2 #Retorna o resto da divisão inteira por 2
+print(x) 
