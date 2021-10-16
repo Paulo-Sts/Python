7e0729f92ddaeca6e06107c2fc10ac88fce1c036
Paulo Santos <pauloed_@outlook.com>
2021-10-16 11:46:31 -0300

    Operadores de associação

---
+++ Operadores/associacao.py
@@ -0,0 +1,9 @@
+#São utilizados para saber se um valor pertence a uma sequência ou não retornando um booleano
+
+carros = ["Fox", "Voyage", "Golf"]
+
+meu_carro = "Fox"
+carro_vizinho = "Palio"
+print(meu_carro in carros) #Pertence
+print(carro_vizinho in carros)
+print(carro_vizinho not in carros) #Não pertence
