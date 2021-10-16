043cd52ae2a6907a183165d11a10d1d5408de5e5
Paulo Santos <pauloed_@outlook.com>
2021-10-16 11:45:28 -0300

    Operadores relacionais

---
+++ Operadores/relacionais.py
@@ -0,0 +1,11 @@
+#Operadores relacionais geram uma comparação entre dois valores e retornam um valor booleano (True ou False)
+
+x = 1
+y = 2
+
+print(x == y) #Igual
+print(x != y) #Diferente de
+print(x < y) #Menor que
+print(x > y) #Maior que
+print(x <= y) #Menor ou igual a
+print(x >= y) #Maior ou igual a
