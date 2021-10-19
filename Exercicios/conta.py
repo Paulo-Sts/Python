b6d57072dfb90075eca8565a1c75b90e3cd0b7d9
Paulo Santos <pauloed_@outlook.com>
2021-10-19 10:14:48 -0300

    Adicionando métodos a classe conta

--- Exercicios/conta.py
+++ Exercicios/conta.py
@@ -6,6 +6,14 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
 
+    def saldo(self):
+        print("Seu saldo é {}".format(self.saldo))
+
+    def deposito(self, valor):
+        self.saldo += valor
+    
+    def saque(self, valor):
+        self.saldo -= valor
 
 conta_1 = Conta(55501, "Paulo", 700.0, 5000.0)
 conta_2 = Conta(55502, "Eduardo", 300.0, 5000.0)
