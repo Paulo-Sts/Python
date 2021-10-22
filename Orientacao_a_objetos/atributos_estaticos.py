4afbe30d22fe095fe3af66ad3bcd47ebd40788b3
Paulo Santos <pauloed_@outlook.com>
2021-10-22 10:28:52 -0300

    Atributos estáticos

---
+++ Orientacao_a_objetos/atributos_estaticos.py
@@ -0,0 +1,10 @@
+#São atributos definidos na classe e que pertencem a classe, independente do objeto
+class Homem:
+    especie = "Homo sapiens" #Atributos de classe podem ser acessados independetemente da existência de objetos do tipo da classe
+    def __init__(self, nome, peso, cor):
+        self.nome = nome
+        self.peso = peso
+        self.cor = cor
+       
+
+print(Homem.especie)
