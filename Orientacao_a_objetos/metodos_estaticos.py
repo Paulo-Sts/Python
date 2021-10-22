724b1167e87e325b089778e64f6ff6da7cbd6d64
Paulo Santos <pauloed_@outlook.com>
2021-10-22 10:29:11 -0300

    metodos estáticos

---
+++ Orientacao_a_objetos/metodos_estaticos.py
@@ -0,0 +1,14 @@
+#São métodos que pertencem a classe independente da existencia de objetos. Para todos os objetos eles serão o mesmo
+class Livro:
+    def __init__(self, nome, autor, preco):
+        self.__nome = nome
+        self.__autor = autor
+        self.__preco = preco
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @staticmethod #Usa-se essa palavra reervada para definir um método estático
+    def livraria():
+        return "Saraiva"
